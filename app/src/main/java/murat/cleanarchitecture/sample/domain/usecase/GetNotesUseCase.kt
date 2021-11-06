0d740bb5209d8ee24615ac5d156e025107055d94
Murat Yüksektepe <yuksektepemurat@gmail.com>
2021-11-06 17:06:35 +0300

    UseCase açıklaması düzenlendi.

--- app/src/main/java/murat/cleanarchitecture/sample/domain/usecase/GetNotesUseCase.kt
+++ app/src/main/java/murat/cleanarchitecture/sample/domain/usecase/GetNotesUseCase.kt
@@ -6,20 +6,20 @@ import murat.cleanarchitecture.sample.domain.model.ResultData
 import murat.cleanarchitecture.sample.domain.repository.NotesRepository
 import javax.inject.Inject
 
-/*
-
-UseCase
-
-Kullanıcının yaptığı her bir işlem için UseCase oluşturulur. Örneğin bütün kayıtların listelenmesi ya da sadece seçilen kaydın detaylarının
-getirilmesi ya da yeni kayıt eklenmesi vb. gibi.
-
-UseCase'ler ilgili Feature için üretilmiş olan Repository'leri çağırarak bu Repository içindeki kendiyle alakalı olan fonksiyondan veri alan,
--genellikle invoke isminde- bir operator fonksiyon barındırır.
-
-TODO Operator Fonksiyon Nedir?
-
- */
+/**
+ *
+ * UseCase
+ *
+ * Uygulamanın içerdiği her bir Feature (özellik)'de kullanıcı için yapılan ya da kullacının bizzat yaptığı
+ * her bir işlem için UseCase oluşturulur.
+ *
+ * Örneğin: Bütün notların listelenmesi, seçilen notun detaylarının getirilmesi ya da yeni not eklenmesi vb. gibi.
+ *
+ * UseCase'ler ilgili Feature (özellik) için üretilmiş olan Repository'leri çağırarak bu Repository içindeki kendiyle
+ * alakalı olan fonksiyondan veri alan, -genellikle invoke (çağırmak) isminde- bir fonksiyon barındırır.
+ *
+ **/
 
 class GetNotesUseCase @Inject constructor(private val respository: NotesRepository) {
-    operator fun invoke(): Flow<ResultData<MutableList<Note>>> = respository.getNotes()
+    fun invoke(): Flow<ResultData<MutableList<Note>>> = respository.getNotes()
 }
